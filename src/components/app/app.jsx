dae39e49bca39d8847890a2405306ed8bca43d2d
Yulia Elagina <julia-elagina@yandex.ru>
2020-06-20 23:13:35 +0300

    Устанавливает react-router-dom и настраивает роутинг в App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,19 +1,43 @@
-import React from "react";
+import React, {PureComponent} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PropTypes from "prop-types";
 import Main from "../main/main.jsx";
 import {headerMovieShape, movieShape} from "../shapes.js";
+import MovieDetails from "../movie-details/movie-details.jsx";
 
 const onMovieTitleClick = () => {};
 
-const App = (props) => {
-  const {headerMovie, movies} = props;
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return <Main
-    headerMovie={headerMovie}
-    movies={movies}
-    onMovieTitleClick={onMovieTitleClick}
-  />;
-};
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderApp()}
+          </Route>
+          <Route exact path="/dev-film">
+            <MovieDetails />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+
+  _renderApp() {
+    const {headerMovie, movies} = this.props;
+
+    return <Main
+      headerMovie={headerMovie}
+      movies={movies}
+      onMovieTitleClick={onMovieTitleClick}
+    />;
+  }
+}
+
 
 App.propTypes = {
   headerMovie: PropTypes.shape(headerMovieShape).isRequired,
