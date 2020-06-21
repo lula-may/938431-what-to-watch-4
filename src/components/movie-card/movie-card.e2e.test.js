9248fdf03ae76297a5d5a31987408d5e56c66310
Yulia Elagina <julia-elagina@yandex.ru>
2020-06-21 22:31:05 +0300

    Добавляет e2e-tests для обработчика клика по карточке

--- src/components/movie-card/movie-card.e2e.test.js
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -17,12 +17,31 @@ describe(`MovieCardComponent`, () => {
         <MovieCard
           movie={movie}
           onMouseEnter={onMouseEnter}
-          onTitleClick={() => {}}
+          onCardClick={() => {}}
         />
     );
 
     movieCard.simulate(`mouseenter`);
     expect(onMouseEnter).toHaveBeenCalledTimes(1);
     expect(onMouseEnter.mock.calls[0][0]).toMatchObject(movie);
+  });
+
+  it(`should run callback on image or title click`, () => {
+    const onCardClick = jest.fn();
+    const movieCard = shallow(
+        <MovieCard
+          movie={movie}
+          onMouseEnter={() => {}}
+          onCardClick={onCardClick}
+        />
+    );
+
+    const imageWrapperElement = movieCard.find(`div.small-movie-card__image`);
+    const titleElement = movieCard.find(`h3`);
+
+    imageWrapperElement.simulate(`click`);
+    titleElement.simulate(`click`);
+
+    expect(onCardClick).toHaveBeenCalledTimes(2);
   });
 });
