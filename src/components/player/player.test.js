77fa3a64f57e85f561f044f2eb302f353fe28979
Yulia Elagina <julia-elagina@yandex.ru>
2020-07-12 23:00:34 +0300

    Создает компонент Player и снепшот-тест к нему

---
+++ src/components/player/player.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import Player from "./player.jsx";
+
+describe(`Player Component`, () => {
+  it(`should render correctly full screen player`, () => {
+    const tree = renderer.create(
+        <Player
+          elapsedTime={`1:30:24`}
+          isPlaying={false}
+          onFullScreenButtonClick={() => {}}
+          onPlayButtonClick={() => {}}
+          progressValue={30}
+        >
+          <video/>
+        </Player>
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
