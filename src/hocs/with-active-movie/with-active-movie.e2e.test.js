078a032e608b0a1ef8240f99c3ba02c1e8ff3d93
Yulia Elagina <julia-elagina@yandex.ru>
2020-07-12 17:12:29 +0300

    Создает е2е-тест для хока WithActiveMovie

---
+++ src/hocs/with-active-movie/with-active-movie.e2e.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {configure, mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import PropTypes from "prop-types";
+import withActiveMovie from "./with-active-movie.jsx";
+import {testMovies} from "../../test-mocks/test-films";
+
+configure({
+  adapter: new Adapter()
+});
+
+const movie = testMovies[0];
+
+const MockComponent = (props) => {
+  const {onMovieCardClick, onMouseEnter} = props;
+  return <div
+    id="mock"
+    onClick={onMovieCardClick}
+    movie={movie}
+    onMouseEnter={() => onMouseEnter(movie)}
+  />;
+};
+MockComponent.propTypes = {
+  onMovieCardClick: PropTypes.func.isRequired,
+  onMouseEnter: PropTypes.func.isRequired,
+};
+
+const WithActiveMovie = withActiveMovie(MockComponent);
+
+describe(`WithActiveMovie HOC`, () => {
+  it(`should get movie from mockComponent on mouse entering and pass it to callback on mockComponent click`, () => {
+    const onMovieCardClick = jest.fn((...args) => [...args]);
+    const wrapper = mount(
+        <WithActiveMovie
+          onMovieCardClick={onMovieCardClick}
+        />
+    );
+    wrapper.find(`#mock`).simulate(`mouseenter`);
+    wrapper.find(`#mock`).simulate(`click`);
+    expect(onMovieCardClick.mock.calls[0][0]).toEqual(movie);
+  });
+});
