515e9ba7320eb4c025b947042bb8e23c2aa4db44
Yulia Elagina <julia-elagina@yandex.ru>
2020-07-12 16:32:56 +0300

    Создает снепшот-тест и обновляет е2е-тест для WithVideoPlayer

---
+++ src/hocs/with-video-player/with-video-player.test.js
@@ -0,0 +1,37 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import PropTypes from "prop-types";
+import withVideoPlayer from "./with-video-player.jsx";
+import {testMovies} from "../../test-mocks/test-films.js";
+
+const movie = testMovies[0];
+
+const MockComponent = (props) => {
+  const {children} = props;
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+MockComponent.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const MockComponentWrapped = withVideoPlayer(MockComponent);
+
+describe(`WithVideoPlayer HOC`, () => {
+  it(`should render <video/> inside MockComponent`, () => {
+    const tree = renderer.create(
+        <MockComponentWrapped
+          movie={movie}
+          onMouseEnter={() => {}}
+        />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
