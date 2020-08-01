03c187dd216f1cb798e71351d3d44fd609945cc3
Yulia Elagina <julia-elagina@yandex.ru>
2020-08-01 20:28:54 +0300

    Обновляет тесты для SignIn

--- src/components/sign-in/sign-in.test.js
+++ src/components/sign-in/sign-in.test.js
@@ -1,15 +1,19 @@
 import React from "react";
+import {Router} from "react-router-dom";
 import renderer from "react-test-renderer";
 import {SignIn} from "./sign-in.jsx";
+import history from "../../history.js";
 
 describe(`SingIn Component`, () => {
   it(`should render correctly SignIn Component`, () => {
     const tree = renderer.create(
-        <SignIn
-          hasLoginError={false}
-          isInvalidEmail={false}
-          onSubmit={() => {}}
-        />
+        <Router history={history}>
+          <SignIn
+            hasLoginError={false}
+            isInvalidEmail={false}
+            onSubmit={() => {}}
+          />
+        </Router>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
@@ -17,11 +21,13 @@ describe(`SingIn Component`, () => {
 
   it(`should render Wrong Email Message in SignIn Component`, () => {
     const tree = renderer.create(
-        <SignIn
-          hasLoginError={true}
-          isInvalidEmail={true}
-          onSubmit={() => {}}
-        />
+        <Router history={history}>
+          <SignIn
+            hasLoginError={true}
+            isInvalidEmail={true}
+            onSubmit={() => {}}
+          />
+        </Router>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
@@ -29,11 +35,13 @@ describe(`SingIn Component`, () => {
 
   it(`should render Upload Error Message in SignIn Component`, () => {
     const tree = renderer.create(
-        <SignIn
-          hasLoginError={true}
-          isInvalidEmail={false}
-          onSubmit={() => {}}
-        />
+        <Router history={history}>
+          <SignIn
+            hasLoginError={true}
+            isInvalidEmail={false}
+            onSubmit={() => {}}
+          />
+        </Router>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
