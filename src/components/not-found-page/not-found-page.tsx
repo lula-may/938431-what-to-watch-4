14f4a59422a9504c33f23695759151da90d93427
Yulia Elagina <julia-elagina@yandex.ru>
2020-08-08 01:41:57 +0300

    Выносит цвет шрифта в константу

--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 
-import {AppRoute} from "../../const";
+import {AppRoute, TEXT_COLOR} from "../../const";
 
 const NotFoundPage: React.FC = () => {
   return (
     <div className="user-page" style={{textAlign: `center`}}>
       <h1>404</h1>
       <h2>Page not found</h2>
-      <Link to={AppRoute.ROOT} style={{color: `#c9b37e`}}>Go to main page</Link>
+      <Link to={AppRoute.ROOT} style={{color: TEXT_COLOR}}>Go to main page</Link>
     </div>
   );
 };
--- src/const.js
+++ src/const.js
@@ -8,12 +8,14 @@ const SIMILAR_MOVIES_COUNT = 4;
 const SHOWED_MOVIES_ON_START_COUNT = 8;
 const DEFAULT_GENRE = `All genres`;
 const MAX_GENRES_COUNT = 9;
+const TEXT_COLOR = `#c9b37e`;
 
 const AppRoute = {
   ROOT: `/`,
   LOGIN: `/login`,
   FILMS: `/films`,
   MY_LIST: `/mylist`,
+  NOT_FOUND: `/notfound`,
   PLAYER: `/player`,
   REVIEW: `/review`,
 };
@@ -26,4 +28,5 @@ export {
   PREVIEW,
   SIMILAR_MOVIES_COUNT,
   SHOWED_MOVIES_ON_START_COUNT,
+  TEXT_COLOR
 };
